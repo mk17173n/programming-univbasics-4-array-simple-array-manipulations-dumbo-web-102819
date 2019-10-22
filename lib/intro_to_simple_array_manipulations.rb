596b1c0d06f1b5c08deda053fe00d69605b48590
Monsur Khan <monsur.khan@gmail.com>
2019-10-22 22:05:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push (array,element)
+array.push(element)
+end
+
+def using_unshift (array,element)
+ array.unshift(element)
+ end
+ 
+ def using_pop (array)
+   array.using_pop
+ end
+ 
+ def pop_with_args(array)
+   array.pop(2)
+ end
+ 
+ 
+ 
+ 
+ 
